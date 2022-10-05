b65a42600a4cba124a43f81ec442730a492c8348
leglerann <leglerann@urbandaleschools.com>
2022-10-04 22:17:05 -0500

    Final change

--- 5.0_Jedi_Training.py
+++ 5.0_Jedi_Training.py
@@ -1,26 +1,23 @@
-  #Sign your name:________________
-
+  # Sign your name: Anna Legler
+import random
 '''
  1. Make the following program work.
    '''
-print("This program takes three numbers and returns the sum.")
-total = 0
-
-for i in range(3):
-    x = int(input("Enter a number: "))
-    total += x
-print("The total is:", total)
-  
+# print("This program takes three numbers and returns the sum.")
+# total = 0
+#
+# for i in range(3):
+#     x = int(input("Enter a number: "))
+#     total += x
+# print("The total is:", total)
 
 
 '''
   2. Write a Python program that will use a FOR loop to print the even
      numbers from 2 to 100, inclusive.
 '''
-for i in range(2, 101, 2):
-    print(i)
-
-
+# for i in range(2, 101, 2):
+#     print(i)
 
 
 '''
@@ -28,19 +25,17 @@ for i in range(2, 101, 2):
      10 down to, and including, 0. Then print the words Blast off! Remember, use
      a WHILE loop, don't use a FOR loop.
 '''
-
-
-
-
+# countdown = 10
+# while countdown >= 0:
+#     print(countdown)
+#     countdown -= 1
+# print("BLAST OFF!!")
 
 
 '''
   4. Write a program that prints a random integer from 1 to 10 (inclusive).
 '''
-
-
-
-
+# print(random.randrange(1, 11))
 
 
 '''
@@ -53,3 +48,18 @@ for i in range(2, 101, 2):
      if statements.
       
 '''
+total = 0
+totalPos = 0
+totalNeg = 0
+totalZero = 0
+for i in range(7):
+    num = int(input("Type a number "))
+    total += num
+    if num > 0:
+        totalPos += 1
+    elif num < 0:
+        totalNeg += 1
+    else:
+        totalZero += 1
+print("The total is", total)
+print("You typed", totalPos, "Positive numbers,", totalNeg, "negative numbers, and", totalZero, "zeros.")
--- 5.1_Coin_Toss.py
+++ 5.1_Coin_Toss.py
@@ -6,8 +6,18 @@ COIN TOSS PROGRAM
 3.) Add a loop so that the program does this 50 times.
 4.) Create a running total for the number of heads and the number of tails and print the total at the end.
 '''
-
-
+import random
+headCount = 0
+tailCount = 0
+for i in range(50):
+    flip = random.randrange(2)
+    if flip == 0:
+        print("Heads")
+        headCount +=1
+    elif flip == 1:
+        print("Tails")
+        tailCount += 1
+print("You flipped heads", headCount, "times, and tails", tailCount, "times.")
 
 
 
